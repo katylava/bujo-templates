0b87b2596cfd4ffdb53c46021a57a7346deae7f4
katy lavallee <katylava@gmail.com>
2017-06-05 17:19:34 -0500

    Add template for days of month

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ var Metalsmith = require('metalsmith')
 var layouts = require('metalsmith-layouts')
 var inplace = require('metalsmith-in-place')
 var sass = require('metalsmith-sass')
+var watch = require('metalsmith-watch')
 
 var _start = moment().startOf('month')
 
@@ -12,17 +13,27 @@ var _start = moment().startOf('month')
 // if it's earlier than the 24th. But I would never wait that long, right?
 var startDate = (new Date).getDate() <= 24 ? _start : _start.add(1, 'month')
 
-var dates = startDate.twix(moment(startDate).endOf('month'), { allDay: true }).toArray('days')
+var monthDates = startDate.twix(moment(startDate).endOf('month'), { allDay: true }).toArray('days')
 
+var monthDateLabels = monthDates.map(d => {
+    return { dow: d.format('dd')[0], dom: d.format('D') }
+})
 
 Metalsmith(__dirname)
     .metadata({
-        date_labels: dates.map(d => d.format('dd D')),
+        month_date_labels: monthDateLabels,
         month_name: moment().format('MMMM YYYY')
     })
     .source('./src')
     .destination('./build')
     .clean(true)
+    .use(watch({
+        paths: {
+            '${source}/**/*': true,
+            'layouts/*': true
+        },
+        livereload: 8080
+    }))
     .use(sass({
         outputDir: 'css/',
         outputStyle: 'nested'
